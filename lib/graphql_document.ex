da1de494941452826fc9479dcf7a46d846460633
Paul Statezny <paulstatezny@gmail.com>
2022-05-13 13:44:20 -0700

    Implement GraphqlDocument.to_string

--- lib/graphql_document.ex
+++ lib/graphql_document.ex
@@ -1,18 +1,119 @@
 defmodule GraphqlDocument do
   @moduledoc """
-  Documentation for `GraphqlDocument`.
+  Utilities for building GraphQL documents, i.e. strings that contain a GraphQL
+  query/mutation.
   """
 
   @doc """
-  Hello world.
+  Generates GraphQL syntax from a nested Elixir keyword list.
 
-  ## Examples
+  ### Example
 
-      iex> GraphqlDocument.hello()
-      :world
+      iex> GraphqlDocument.to_string(query: [user: {[id: 3], [:name, :age, :height, documents: [:filename, :url]]}])
+      \"\"\"
+      query {
+        user(id: 3) {
+          name
+          age
+          height
+          documents {
+            filename
+            url
+          }
+        }
+      }\\
+      \"\"\"
 
   """
-  def hello do
-    :world
+  def to_string(params, indent_level \\ 0)
+
+  def to_string(params, indent_level) when is_list(params) or is_map(params) do
+    indent = String.duplicate("  ", indent_level)
+
+    params
+    |> Enum.map(fn
+      {field, {[] = _args, sub_fields}} ->
+        # empty arguments were passed in; remove them because `field() { subfield }` isn't valid GraphQL
+        {field, sub_fields}
+
+      {field, {%{} = args, sub_fields}} when map_size(args) == 0 ->
+        # empty arguments were passed in; remove them because `field() { subfield }` isn't valid GraphQL
+        {field, sub_fields}
+
+      field ->
+        field
+    end)
+    |> Enum.map_join("\n", fn
+      field when is_binary(field) or is_atom(field) ->
+        "#{indent}#{field}"
+
+      {field, {args, sub_fields}} when is_map(args) or is_list(args) ->
+        args_string =
+          args
+          |> Enum.map_join(", ", fn {key, value} ->
+            "#{key}: #{argument(value)}"
+          end)
+
+        sub_fields_string =
+          if Enum.any?(sub_fields) do
+            " {\n#{to_string(sub_fields, indent_level + 1)}\n#{indent}}"
+          else
+            ""
+          end
+
+        "#{indent}#{field}(#{args_string})#{sub_fields_string}"
+
+      {field, {args, _sub_fields}} ->
+        raise "Expected a keyword list or map for args for field #{inspect(field)}, received: #{inspect(args)}"
+
+      {field, [] = _sub_fields} ->
+        "#{indent}#{field}"
+
+      {field, sub_fields} ->
+        "#{indent}#{field} {\n#{to_string(sub_fields, indent_level + 1)}\n#{indent}}"
+    end)
+  end
+
+  def to_string(params, _indent_level) do
+    raise RuntimeError,
+      message: """
+      [GraphqlDocument] Expected a list of fields but received `#{inspect(params)}`.
+
+      Did you forget to enclose it in a list?
+      """
+  end
+
+  defp argument({:enum, enum}), do: String.upcase(enum)
+  defp argument(%Date{} = date), do: inspect(Date.to_iso8601(date))
+  defp argument(%DateTime{} = date_time), do: inspect(DateTime.to_iso8601(date_time))
+  defp argument(%Time{} = time), do: inspect(Time.to_iso8601(time))
+
+  defp argument(%NaiveDateTime{} = naive_date_time),
+    do: inspect(NaiveDateTime.to_iso8601(naive_date_time))
+
+  if Code.ensure_loaded?(Decimal) do
+    defp argument(%Decimal{} = decimal), do: Decimal.to_string(decimal)
+  end
+
+  defp argument([]), do: "[]"
+
+  defp argument(enum) when is_list(enum) or is_map(enum) do
+    if is_map(enum) || Keyword.keyword?(enum) do
+      nested_arguments =
+        enum
+        |> Enum.map_join(", ", fn {key, value} -> "#{key}: #{argument(value)}" end)
+
+      "{#{nested_arguments}}"
+    else
+      nested_arguments =
+        enum
+        |> Enum.map_join(", ", &"#{argument(&1)}")
+
+      "[#{nested_arguments}]"
+    end
+  end
+
+  defp argument(value) do
+    inspect(value, printable_limit: :infinity)
   end
 end
--- test/graphql_document_test.exs
+++ test/graphql_document_test.exs
@@ -2,7 +2,195 @@ defmodule GraphqlDocumentTest do
   use ExUnit.Case
   doctest GraphqlDocument
 
-  test "greets the world" do
-    assert GraphqlDocument.hello() == :world
+  describe "to_string/1" do
+    test "builds a GraphQL syntax string from an Elixir data structure" do
+      result =
+        GraphqlDocument.to_string(
+          query: [
+            invoices:
+              {[customer: "123456"],
+               [
+                 :id,
+                 :total,
+                 items: ~w(description amount),
+                 payments: {
+                   [after: "2021-01-01"],
+                   ~w(amount date)
+                 }
+               ]}
+          ]
+        )
+
+      expected = """
+      query {
+        invoices(customer: "123456") {
+          id
+          total
+          items {
+            description
+            amount
+          }
+          payments(after: "2021-01-01") {
+            amount
+            date
+          }
+        }
+      }\
+      """
+
+      assert result == expected
+    end
+
+    test "it's possible to build a query for a mutation that returns a scalar rather than an object" do
+      result =
+        GraphqlDocument.to_string(
+          mutation: [
+            launch_rockets: {[where: "outer space"], []}
+          ]
+        )
+
+      expected = """
+      mutation {
+        launch_rockets(where: "outer space")
+      }\
+      """
+
+      assert result == expected
+
+      result =
+        GraphqlDocument.to_string(
+          query: [
+            getThings: {
+              [
+                zip: "123",
+                test: %{
+                  "bar" => "ASDF",
+                  "nested" => %{
+                    baz: "bazzz"
+                  }
+                }
+              ],
+              [:city]
+            }
+          ]
+        )
+
+      expected = """
+      query {
+        getThings(zip: "123", test: {bar: "ASDF", nested: {baz: "bazzz"}}) {
+          city
+        }
+      }\
+      """
+
+      assert result == expected
+    end
+
+    test "nested arguments are supported" do
+      result =
+        GraphqlDocument.to_string(
+          mutation: [
+            launch_rockets: {
+              [when: %{day: "tomorrow", time: [hour: 9, minute: 3, second: 30]}],
+              [:status]
+            }
+          ]
+        )
+
+      expected = """
+      mutation {
+        launch_rockets(when: {day: "tomorrow", time: {hour: 9, minute: 3, second: 30}}) {
+          status
+        }
+      }\
+      """
+
+      assert result == expected
+    end
+
+    test "empty args" do
+      result =
+        GraphqlDocument.to_string(
+          query: [{"app_stats", {[], [:registrations, :logins, :complaints]}}]
+        )
+
+      expected = """
+      query {
+        app_stats {
+          registrations
+          logins
+          complaints
+        }
+      }\
+      """
+
+      assert result == expected
+    end
+
+    test "return values that are an empty list are ignored" do
+      result = GraphqlDocument.to_string(mutation: [launch_rockets: {[when: "now"], []}])
+
+      expected = """
+      mutation {
+        launch_rockets(when: "now")
+      }\
+      """
+
+      assert result == expected
+
+      result = GraphqlDocument.to_string(mutation: [launch_rockets: {[], []}])
+
+      expected = """
+      mutation {
+        launch_rockets
+      }\
+      """
+
+      assert result == expected
+    end
+
+    test "pass enum argument as all caps" do
+      result =
+        GraphqlDocument.to_string(
+          query: [
+            get_rockets: {
+              [rocket_type: MASSIVE],
+              [:status]
+            }
+          ]
+        )
+
+      expected = """
+      query {
+        get_rockets(rocket_type: MASSIVE) {
+          status
+        }
+      }\
+      """
+
+      assert result == expected
+    end
+
+    test "pass enum argument as an {:enum, string} tuple" do
+      result =
+        GraphqlDocument.to_string(
+          query: [
+            get_rockets: {
+              [rocket_type: {:enum, "massive"}],
+              [:status]
+            }
+          ]
+        )
+
+      expected = """
+      query {
+        get_rockets(rocket_type: MASSIVE) {
+          status
+        }
+      }\
+      """
+
+      assert result == expected
+    end
   end
 end
